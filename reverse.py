a2426baad2fffc892e598541ffbbe04b6cfb1ddd
kc8son <kc8son@gmail.com>
2020-02-03 13:07:42 -0600

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,7 +1,11 @@
+######################################################################
+#   Date Modified: 02/03/2020   By: kc8son
+#   Fixed the reverse() function.  Adding the index [::-1] will start 
+#   at the end of the string and run through it backwards.
 import sys
 
 def reverse(in_string):
-  return in_string
+  return in_string[::-1]
   
   
 print("This is " + sys.argv[0])
